fb9e659ddc46e7a84a4b2d096bec706a5c830c80
bkachigunda <Barbara.Kachigunda@curtin.edu.au>
2025-04-03 14:56:49 +0800

    Compute the range values

--- goostats.sh
+++ goostats.sh
@@ -1,6 +1,8 @@
+
 #Load a given file
+fname ($F1)
 echo "working with Barbara 's file"
 echo "Welcome to Barbara's stats script"
 #Compute the min/max/range of value in a file
 #Max = 
-
+range =$( echo "Max-Min)
